fe172db1429a1bac07e98f5e3574f03ec7ae1414
scwyh <scwyh@qq.com>
2018-04-23 20:49:43 +0800

    修改视图

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel 5.6
                 </div>
 
                 <div class="links">
